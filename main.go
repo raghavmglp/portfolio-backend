e61a4cd552364399db1b45db96b5679307efa67b
raghavmglp <raghavmtests@gmail.com>
2025-12-31 19:16:14 +0100

    added authorization to allowHeaders options

--- main.go
+++ main.go
@@ -23,12 +23,12 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://raghavm.dev"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
-
+	s
 	routes.RegisterPostRoutes(r, postHandler)
 
 	r.Run(":8080")
